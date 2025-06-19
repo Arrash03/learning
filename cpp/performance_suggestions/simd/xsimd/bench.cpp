6cbcb78d08485a1902d6552cf096505f971c262e
Arrash Tahery <atv1382@gmail.com>
2025-06-19 16:22:11 +0330

    Add xsimd bench

--- cpp/performance_suggestions/simd/xsimd/bench.cpp
+++ cpp/performance_suggestions/simd/xsimd/bench.cpp
@@ -1,17 +1,56 @@
+#include <cstdint>
 #include <xsimd/xsimd.hpp>
-#include <iostream>
 
-int main() {
-    using batch = xsimd::batch<float>;
-    float a[8] = {1,2,3,4,5,6,7,8};
-    float b[8] = {8,7,6,5,4,3,2,1};
-    float c[8];
+#include <benchmark/benchmark.h>
 
-    batch va = batch::load_unaligned(a);
-    batch vb = batch::load_unaligned(b);
-    batch vc = va + vb;
-    vc.store_unaligned(c);
+static constexpr size_t container_size = 8;
+static constexpr size_t iterations = 1'000'000'000;
 
-    for (float f : c) std::cout << f << " ";
-    std::cout << "\n";
+using a = uint64_t[container_size];
+
+extern void __attribute__((noinline)) fill_data(a& container);
+
+static void simd_sum(benchmark::State& state)
+{
+    using batch = xsimd::batch<uint64_t>;
+    uint64_t a[container_size];
+    uint64_t b[container_size];
+    fill_data(a);
+    fill_data(b);
+    uint64_t result[container_size];
+
+    for (auto _ : state)
+    {
+        batch va = batch::load_unaligned(a);
+        batch vb = batch::load_unaligned(b);
+        batch vc = va + vb;
+        vc.store_unaligned(result);
+        benchmark::DoNotOptimize(result);
+    }
 }
+
+BENCHMARK(simd_sum)
+    ->Unit(benchmark::kNanosecond)
+    ->Iterations(iterations);
+
+static void normal_sum(benchmark::State& state)
+{
+    uint64_t a[container_size];
+    uint64_t b[container_size];
+    fill_data(a);
+    fill_data(b);
+    uint64_t result[container_size];
+
+    for (auto _ : state)
+    {
+        for(size_t i=0; i < container_size; i++)
+            result[i] = a[i] + b[i];
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+BENCHMARK(normal_sum)
+    ->Unit(benchmark::kNanosecond)
+    ->Iterations(iterations);
+
+BENCHMARK_MAIN();
