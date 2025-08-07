7857c7b86a35847bcc27848c03587f8f1e2a3e48
Arrash Tahery <atv1382@gmail.com>
2025-08-07 14:42:38 +0330

    some real examples for simd operations(simd_comparator)

---
+++ cpp/performance_suggestions/simd/xsimd/comparator/real_examples/normal.cpp
@@ -0,0 +1,51 @@
+#include <cstdint>
+#include <random>
+#include <cstring>
+#include <iostream>
+
+static constexpr size_t size = 1ULL << 26;
+
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<uint64_t> distrib(0, std::numeric_limits<uint64_t>::max());
+
+static void a(uint64_t ** src, uint64_t** dst)
+{
+    // *src = new uint64_t[size];
+    // *dst = new uint64_t[size];
+    *src = static_cast<uint64_t*>(std::aligned_alloc(64, size * sizeof(uint64_t)));
+    *dst = static_cast<uint64_t*>(std::aligned_alloc(64, size * sizeof(uint64_t)));
+    for(size_t i = 0; i < size; ++i)
+    {
+        (*src)[i] = distrib(gen);
+    }
+
+    std::memcpy(*dst, *src, size * sizeof(uint64_t));
+    // Intentionally modify the last byte to create a difference
+}
+
+bool __attribute__((noinline)) cmp(const uint64_t* a, const uint64_t* b, size_t size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    uint64_t* src;
+    uint64_t* dst;
+    a(&src, &dst);
+    
+    // Example usage of the comparison function
+    bool result = cmp(dst, src, size);
+
+    std::cout << result << std::endl;
+    
+    // Clean up
+    delete [] src;
+    delete [] dst;
+}
