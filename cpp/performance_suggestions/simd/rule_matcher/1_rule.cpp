87b536f0f3ed9e75bb7b71c8294b80c6ff28216f
Arrash Tahery <atv1382@gmail.com>
2025-11-27 17:09:34 +0330

    remove unrolling from 1_rule

--- cpp/performance_suggestions/simd/rule_matcher/1_rule.cpp
+++ cpp/performance_suggestions/simd/rule_matcher/1_rule.cpp
@@ -11,29 +11,6 @@ static constexpr size_t iterations = 1'000'00;
 static constexpr size_t rows = 256;
 static constexpr size_t columns = 1500;
 
-#define quad_loop(start, step, end)\
-    for (; (start) + 4 * (step) - 1 < end; (start) += 4 * (step)) {\
-        work((start))\
-        work((start) + (step))\
-        work((start) + 2 * (step))\
-        work((start) + 3 * (step))\
-    }
-
-#define square_loop(start, step, end)\
-    for (; (start) + 2 * (step) - 1 < end; (start) += 2 * (step)) {\
-        work((start))\
-        work((start) + (step))\
-    }
-
-#define unrolling_loop(start, step, end)\
-    quad_loop((start), (step), (end))\
-    square_loop((start), (step), (end))\
-    if ((start) + (step) - 1 < (end)) {\
-        work((start))\
-        (start) += (step);\
-    }
-
-
 struct Rule
 {
     size_t offset;
@@ -102,18 +79,15 @@ bool match_simd_array_normal(const td_array a, size_t size, const volatile Rule&
 {
     size_t i = 0;
 
-    #define work(i)\
-    do {\
-        __m512i va = _mm512_set1_epi64(rule.vlaue);\
-        __m512i vb = _mm512_set_epi64(a[i][rule.offset], a[i + 1][rule.offset], a[i + 2][rule.offset], a[i + 3][rule.offset],\
-                    a[i + 4][rule.offset], a[i + 5][rule.offset], a[i + 6][rule.offset], a[i + 7][rule.offset]);\
-        __mmask64 cmp = _mm512_cmpeq_epi8_mask(va, vb);\
-        benchmark::DoNotOptimize(cmp);\
-    } while (0);
-
-    unrolling_loop(i, 8, size);
-
-    #undef work
+    while (i < size - 7)
+    {
+        __m512i va = _mm512_set1_epi64(rule.vlaue);
+        __m512i vb = _mm512_set_epi64(a[i][rule.offset], a[i + 1][rule.offset], a[i + 2][rule.offset], a[i + 3][rule.offset],
+                    a[i + 4][rule.offset], a[i + 5][rule.offset], a[i + 6][rule.offset], a[i + 7][rule.offset]);
+        __mmask64 cmp = _mm512_cmpeq_epi8_mask(va, vb);
+        benchmark::DoNotOptimize(cmp);
+        i += 8;
+    }
 
     while (i < size)
     {
